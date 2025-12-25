1cfeef771cdb425751a2a3866a103c0c1761f7b1
IamPhil <63304710+IamPhilG@users.noreply.github.com>
2025-12-25 08:58:19 -0500

    ajoute les imports S3

--- backend/api-node/src/handlers/api.mjs
+++ backend/api-node/src/handlers/api.mjs
@@ -1,6 +1,8 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { v4 as uuidv4 } from 'uuid';
+import { S3Client, PutObjectCommand, ListObjectsV2Command, DeleteObjectCommand } from '@aws-sdk/client-s3';
+
 
 const s3Client = new S3Client({});
 
